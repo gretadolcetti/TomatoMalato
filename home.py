6db16ae41202c366a058e3e7d8a692b92d81452c
Greta Dolcetti <53076361+gretadolcetti@users.noreply.github.com>
2022-11-13 19:37:23 +0100

    Use hash to save file only if not already present

--- home.py
+++ home.py
@@ -2,10 +2,11 @@ import os
 from flask import Flask, flash, request, redirect, render_template, send_from_directory
 from flask import Flask
 import random
+import json
+import hashlib
 
 # UPLOAD_FOLDER: where to save the uploaded files (temporarily?)
 UPLOAD_FOLDER = os.path.join(".","static","uploads")
-print("UPLOAD_FOLDER: ",UPLOAD_FOLDER)
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
@@ -28,9 +29,18 @@ def upload_file():
             flash('Nessun file selezionato')
             return render_template('index.html')
         
+        images_hash = open(os.path.join(".","static","uploads","images_hash.json")).read()
+        images_hash = json.loads(images_hash)
+
+        img_key = hashlib.md5(file.read()).hexdigest()
+        
         if file and allowed_file(file.filename):
-            # Save image in the uploads folder
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            # Save image in the uploads folder if it is not already present
+            if img_key not in images_hash:
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], img_key))
+                images_hash[img_key] = file.filename
+                with open(os.path.join(".","static","uploads","images_hash.json"), "w") as f:
+                    f.write(json.dumps(images_hash))
             risultato = random.choice(["sana", "affetta da A", "affetta da B", "affetta da C"])
             return render_template('index.html', name=os.path.join(app.config['UPLOAD_FOLDER'], file.filename), risultato=risultato)
         elif file and not allowed_file(file.filename):
