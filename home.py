1e793121e5025809dccfd51b62224fedd52e54d2
Greta Dolcetti <53076361+gretadolcetti@users.noreply.github.com>
2022-11-13 18:42:45 +0100

    Simple page example

--- home.py
+++ home.py
@@ -1,16 +1,20 @@
 import os
 from flask import Flask, flash, request, redirect, render_template, send_from_directory
 from flask import Flask
-from flask.ext.hashing import Hashing
+import random
 
+# UPLOAD_FOLDER: where to save the uploaded files (temporarily?)
+UPLOAD_FOLDER = os.path.join(".","static","uploads")
+print("UPLOAD_FOLDER: ",UPLOAD_FOLDER)
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-UPLOAD_FOLDER = '.\image'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app = Flask(__name__, static_url_path='/static')
 
-app = Flask(__name__)
-hashing = Hashing(app)
+# Secret key needed to use flash
+app.secret_key = "super secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -19,22 +23,16 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
-        print(request.files)
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
-        filename_hash = hashing.hash_value(file, salt='abcd')
-
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
+        if file.filename == '':
+            flash('Nessun file selezionato')
+            return render_template('index.html')
         
         if file and allowed_file(file.filename):
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename_hash))
-            return render_template('index.html', name='/image/'+filename_hash)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            risultato = random.choice(["sana", "affetta da A", "affetta da B", "affetta da C"])
+            return render_template('index.html', name=os.path.join(app.config['UPLOAD_FOLDER'], file.filename), risultato=risultato)
+        elif file and not allowed_file(file.filename):
+            flash('Estensione non ammessa')
+            return render_template('index.html')
     return render_template('index.html')
-
-@app.route('/image/<string:image>')
-def send_report(image):
-    print(image)
-    return send_from_directory('image', image)
